730d0a339fe7392b0cc8aa0b493eb734140dc0c3
Niji Ano <fab-bertheol@hotmail.fr>
2024-12-15 17:52:01 +0100

    pylint fix

--- api.py
+++ api.py
@@ -210,5 +210,5 @@ if __name__ == "__main__":
         app.run(host="0.0.0.0", port=8008, debug=True)
 
     except OSError as e:
-        # pylint: disable=W1201
+        # pylint: disable=W1201,W1203
         logging.error(f"Server error: {e}")
