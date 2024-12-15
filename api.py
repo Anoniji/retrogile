5c4fd1cab4c4fe05d274eb1c54f6dd0660663d45
Niji Ano <fab-bertheol@hotmail.fr>
2024-12-15 17:32:54 +0100

    pylint fix

--- api.py
+++ api.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, send_from_directory
-from flask_jsonpify import jsonify
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import os
 import json
 import uuid
 import logging
+
+from flask import Flask, render_template, send_from_directory
+from flask_jsonpify import jsonify
 
 from gevent import monkey
 monkey.patch_all()
@@ -18,12 +21,18 @@ app = Flask(__name__, template_folder="./pages/")
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    if os.path.isfile("index.html"):
+        return render_template("index.html")
+
+    return jsonify(["index_not_found"])
 
 
 @app.route("/LICENSES")
 def licenses():
-    return render_template("licenses.html")
+    if os.path.isfile("licenses.html"):
+        return render_template("licenses.html")
+
+    return jsonify(["licenses_not_found"])
 
 
 @app.route("/create_board/<string:board_name>/<string:author>")
@@ -32,7 +41,7 @@ def create_board(board_name, author):
     if not os.path.isdir(board_dir):
         os.mkdir(board_dir)
 
-    board_id = uuid.uuid4().hex
+    board_id = str(uuid.uuid4().hex)
     board_filename = f'{board_id}.json'
     with open(f'{board_dir}{board_filename}', 'w') as fichier:
         json.dump({
@@ -52,44 +61,42 @@ def create_board(board_name, author):
 
 @app.route("/board/")
 def home():
-    return render_template("home.html")
+    if os.path.isfile("home.html"):
+        return render_template("home.html")
+
+    return jsonify(["board_not_found"])
+
 
 @app.route("/board/<string:board_id>")
 def board(board_id):
-    return render_template("board.html")
+    if os.path.isfile("board.html"):
+        return render_template("board.html")
+
+    return jsonify(["board_not_found"])
 
 
 @app.route("/js/<string:path>", methods=["GET"])
-def JS(path):
+def js(path):
     if os.path.isfile("js/" + path):
-        try:
-            return send_from_directory(
-                "js", path, mimetype="application/javascript"
-            )
-        except Exception as e:
-            return jsonify(["send_from_directory_error: " + str(e)])
+        return send_from_directory(
+            "js", path, mimetype="application/javascript"
+        )
 
     return jsonify(["js_not_found"])
 
 
 @app.route("/css/<string:path>", methods=["GET"])
-def CSS(path):
+def css(path):
     if os.path.isfile("css/" + path):
-        try:
-            return send_from_directory("css", path, mimetype="text/css")
-        except Exception as e:
-            return jsonify(["send_from_directory_error: " + str(e)])
+        return send_from_directory("css", path, mimetype="text/css")
 
     return jsonify(["css_not_found"])
 
 
 @app.route("/img/<string:path>", methods=["GET"])
-def PNG(path):
+def png(path):
     if os.path.isfile("img/" + path):
-        try:
-            return send_from_directory("img/", path, mimetype="image/png")
-        except Exception as e:
-            return jsonify(["send_from_directory_error: " + str(e)])
+        return send_from_directory("img/", path, mimetype="image/png")
 
     return jsonify(["png_not_found"])
 
@@ -99,7 +106,7 @@ def PNG(path):
 
 if __name__ == "__main__":
     try:
-        app.run(host="0.0.0.0", port=8008, debug=False)
+        app.run(host="0.0.0.0", port=8008, debug=True)
 
     except OSError as e:
         logging.error(f"Server error: {e}")
--- ws.py
+++ ws.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
 import asyncio
 import datetime
 import websockets
