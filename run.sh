4e45e729e4da550e5819eb0f346e6ddc495101d8
Niji Ano <fab-bertheol@hotmail.fr>
2025-12-31 22:21:40 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,18 @@
 #!/bin/sh
 
+WS_SUBDOMAIN="wss."
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --ws_subdomain)
+      WS_SUBDOMAIN="$2"
+      shift 2
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+echo "WS_SUBDOMAIN: $WS_SUBDOMAIN"
 exec python api.py &
-exec python ws.py --ws_subdomain "wss."
+exec python ws.py --ws_subdomain "$WS_SUBDOMAIN"
