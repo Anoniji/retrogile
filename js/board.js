841e781ab52c7a24760809e25be71c2ba5868423
Niji Ano <fab-bertheol@hotmail.fr>
2026-01-06 07:46:15 +0100

    Update board.js

--- js/board.js
+++ js/board.js
@@ -1009,8 +1009,8 @@ if (username !== null) {
 
                 $.each(board_data, function (index, value) {
                     var h1_name = index;
-                    if (h1_name.length > 20) {
-                        h1_name = h1_name.substring(0, 20) + '...';
+                    if (h1_name.length > 18) {
+                        h1_name = h1_name.substring(0, 18) + '...';
                     }
 
                     html = `<div id='col_${index}' data-col='${index}' class='${col_class}'><h1>${h1_name}<i onclick='addCard("${index}");' class='add_icon material-icons'>add</i>`;
