25eeca3a7f32ae8b6643bd8de4d786715bd086d2
Niji Ano <fab-bertheol@hotmail.fr>
2026-01-07 20:27:39 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-exec python api.py --ws_subdomain "${WS_SUBDOMAIN:-wss.}" --account_email "${ACCOUNT_EMAIL}" &
-exec python ws.py
+exec python api.py --ws_subdomain "${WS_SUBDOMAIN:-wss.}" --account_email "${ACCOUNT_EMAIL:-demo@retrogile.com}" &
+exec python ws.py --account_email "${ACCOUNT_EMAIL:-demo@retrogile.com}"
