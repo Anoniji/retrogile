2b9aa055afc328ebef2866447040152807dffa76
Niji Ano <fab-bertheol@hotmail.fr>
2026-01-08 17:58:07 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-echo $WS_SUBDOMAIN
-echo $ACCOUNT_EMAIL
-
 exec python api.py --ws_subdomain $WS_SUBDOMAIN --account_email $ACCOUNT_EMAIL &
 exec python ws.py --account_email $ACCOUNT_EMAIL
